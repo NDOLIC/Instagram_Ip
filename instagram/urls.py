d5725d6825a9e1027348793d78f80800e6c37e4f
NDOLIC <tblaguese1@gmail.com>
2019-03-26 11:41:52 +0200

    add new file

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,4 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('instapp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/claire/Desktop/Instagram_Ip/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
